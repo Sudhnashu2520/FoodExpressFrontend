abc7f429166f72a8b2f980f8d74fb7135d9c2ad9
Sudhanshu2520 <sudhanshulokhande768@gmail.com>
2024-09-02 03:07:07 +0530

    changes

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://foodexpressbackend-k8u1.onrender.com"
+    const url = "https://foodexpressbackend-k8u1.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
